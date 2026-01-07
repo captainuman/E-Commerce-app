b1631024c28cb3b2fc99d38d64a2fdc784b072d6
Numan <kingnuman2611@gmail.com>
2026-01-07 14:33:09 +0530

    updated images

--- src/Components/homepage/navbar.jsx
+++ src/Components/homepage/navbar.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useRef, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import {Link} from 'react-router-dom'
 import { Itemsdata } from '../data/api'
 
@@ -21,7 +21,7 @@ const Navbar = () => {
             <div className='fixed left-250 top-0 w-70 h-screen z-10 bg-gray-100'
             onClick={()=>setProfile(false)}>
                 <div className='flex gap-2 items-center h-15 w-fit px-10'>
-                    <img className='w-20 h-10' src="/src/Components/data/home.png" alt="Captain"/>
+                    <img className='w-20 h-10' src="/images/home.png" alt="Captain"/>
                     <h1 className='text-2xl font-bold'>Cell Zone</h1>
                 </div>
                 <ul className="flex flex-col gap-5 px-2 py-5">
